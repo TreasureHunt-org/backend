67e20c574bfdfe1cbdc6ae063ba25bade2ebb39d
Rashed Maaitah <bahran568@gmail.com>
2025-03-04 17:15:32 +0300

    init commit

---
+++ src/main/java/org/treasurehunt/TreasureApiApplication.java
@@ -0,0 +1,13 @@
+package org.treasurehunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreasureApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/treasurehunt/TreasureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.treasurehunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
